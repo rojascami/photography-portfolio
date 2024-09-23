06ab8ab58158583b940489c81d6dd9e2198a04e3
Camila Rojas <68752568+rojascami@users.noreply.github.com>
2024-09-23 12:25:47 -0300

    Update video.jsx

--- src/components/VideoNav/video.jsx
+++ src/components/VideoNav/video.jsx
@@ -6,11 +6,13 @@ export default async function VideosNav({videos}) {
   return (
     <div className={styles.wrapper}>
       
-  { videos.map((item) => (
+  { videos.map((item, index) => (
+     <div key={index}>
       <video className={styles.figure} src={item.src} width="320" height="240" controls preload="none">
       <source className={styles.figure} src={item.src} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
+   </div>
   ))}
   </div>)
 }
