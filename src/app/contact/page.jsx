f9c649bef66e4cf5158a088e04ad4b73851e104a
XE064256259_EYGS <camilablitz@gmail.com>
2024-09-26 16:41:38 -0300

    footer

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,6 @@
 import styles from "./style.module.scss";
-
 import Circle from "../../components/Circle/circle";
-import Footer from "../../components/Footer/footer";
+import Footer from "../../components/Footer/Footer"; // Corrected import statement
 
 export default function Home() {
   return (
